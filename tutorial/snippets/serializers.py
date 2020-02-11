92054017005d8bef2a38049703556bd66e09110d
Ojas Joshi <ojasjoshi@ojass-mbp.audetemi.com>
2020-02-11 15:55:23 +0530

    added hyperlinking our api

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -2,14 +2,15 @@ from rest_framework import serializers
 
 #from .models import Snippet
 from .models import Snippet
+from django.contrib.auth.models import User
 
-
+"""
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
         fields = ['id','username','firstname','lastname', 'title', 'code', 'linenos', 'language', 'style']
 
-from django.contrib.auth.models import User
+
 
 
 #Adding endpoints for our User models
@@ -21,3 +22,23 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'snippets']
         #Updating our serializer
         owner = serializers.ReadOnlyField(source='owner.username')
+
+#Hyperlinking our API
+"""
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
+    class Meta:
+        model = Snippet
+        fields = ['url', 'id', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style']
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['url', 'id', 'username', 'snippets']
